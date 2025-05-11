62e28dc3391196f0b6c22023da481d9ec8e7dea4
prateeksha-yadav <prateekshayadav020@gmail.com>
2025-05-11 22:05:24 +0530

    Add build

---
+++ build/precache-manifest.84e0a686c4393c29f3c71c2322b888a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade93705587fec187a37cc05325508ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc07cc1945c0bddbda4d",
+    "url": "/static/css/main.904f238b.chunk.css"
+  },
+  {
+    "revision": "33587fe8c69220dd47b9",
+    "url": "/static/js/2.6b8e911b.chunk.js"
+  },
+  {
+    "revision": "80c22b709854abab6800481175e1e427",
+    "url": "/static/js/2.6b8e911b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc07cc1945c0bddbda4d",
+    "url": "/static/js/main.0006807a.chunk.js"
+  },
+  {
+    "revision": "560f2fdfcbb2dbc77e00",
+    "url": "/static/js/runtime-main.3ef9a65c.js"
+  }
+]);
